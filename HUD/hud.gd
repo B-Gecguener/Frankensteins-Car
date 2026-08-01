d990b9f3e0950097a8187fc88f5a2fe5906bddd1
Batu <batu@pool8021x171-86.RZ.HS-Augsburg.DE>
2026-08-01 14:40:44 +0200

    hud implemented

--- HUD/hud.gd
+++ HUD/hud.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 
-@onready var auto: Player = $"."
-@onready var label: Label = $Control/Label
-@onready var progress_bar: ProgressBar = $Control/ProgressBar
+@onready var auto: Player = $"../Auto"
+@onready var scrap_label: Label = $Control/scrap_label
+
+@onready var energy_health: ProgressBar = $Control/energy_progressbar
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	label.text = str(auto.battery) + "/" + str(auto.max_car_battery)
+	scrap_label.text = str(auto.scrap)
+	energy_health.value = auto.car_battery
