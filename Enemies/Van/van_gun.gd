3482471ac2896b023df0d5d9ad7885c29eb2099e
Marco <133157182+ProfessorClever@users.noreply.github.com>
2026-08-02 13:15:55 +0200

    van bullet spray

--- Enemies/Van/van_gun.gd
+++ Enemies/Van/van_gun.gd
@@ -10,7 +10,7 @@ var projectile: PackedScene = load("res://Objects/projectileVan.tscn")
 
 @export_group("Aiming")
 @export var max_yaw_speed: float = 7       # rad/s, top traverse speed
-@export var yaw_accel: float = 10            # rad/sÂ², lower = more momentum
+@export var yaw_accel: float = 3            # rad/sÂ², lower = more momentum
 @export var tracking_gain: float = 8.0       # how aggressively it closes the error
 
 var yaw_speed: float = 0.0
@@ -20,14 +20,14 @@ var loaded: bool = true
 var active: bool = true
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-var randomness: Vector3 = Vector3(rng.randf_range(0.0,3.0),0,rng.randf_range(0.0,3.0))
+var randomness: Vector3 = Vector3(rng.randf_range(0.0,10.0),0,rng.randf_range(0.0,10.0))
 
 func _ready() -> void:
 	_recalc_randomness()
 
 func _recalc_randomness() -> void:
 	get_tree().create_timer(1.0,false,true,false).timeout.connect(_recalc_randomness)
-	randomness = Vector3(rng.randf_range(0.0,2.0),0,rng.randf_range(0.0,2.0))
+	randomness = Vector3(rng.randf_range(0.0,8.0),0,rng.randf_range(0.0,8.0))
 
 func _physics_process(delta: float) -> void:
 	if active:
@@ -41,7 +41,6 @@ func _physics_process(delta: float) -> void:
 			var target = car.global_position + vorhalt + (randomness * speed)
 
 			var yaw_error: float = angle_difference(turret.rotation.y, _target_yaw(target))
-			5
 			desired_yaw_speed = clampf(yaw_error * tracking_gain, -max_yaw_speed, max_yaw_speed)
 	
 			if loaded:
@@ -54,7 +53,7 @@ func _physics_process(delta: float) -> void:
 func fire() -> void:
 	var bullet: Projectile = projectile.instantiate()
 	add_child(bullet)
-	bullet.flight_vector = (-muzzle.global_basis.z).normalized()
+	bullet.flight_vector = (-muzzle.global_basis.z).normalized().rotated(Vector3(0,1,0),rng.randf_range(-0.2,0.2))
 	bullet.global_position = muzzle.global_position
 	loaded = false
 	get_tree().create_timer(0.1).timeout.connect(reload)
